89fdf72f10e8f2c3054fbda091e81d2c7c723077
chenlinyue <chenlinyue1@gmail.com>
2018-03-27 11:23:11 +0800

    Create MoveZeroes_283.py

---
+++ 200-299/MoveZeroes_283.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        count = 0
+        while i < len(nums):
+            if nums[i] == 0:
+                del nums[i]     # while using del, nums change and automatically len(nums) too
+                count += 1
+            else:
+                i += 1
+        nums += [0] * count   # extend nums with correct number of 0
