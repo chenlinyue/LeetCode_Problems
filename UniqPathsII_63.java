0480c112e8d9f5fff9c09e09e0f9dacec93fa445
chenlinyue <chenlinyue1@gmail.com>
2019-03-07 20:27:58 +0800

    Create UniqPathsII_63.java

---
+++ UniqPathsII_63.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] result = new int[m][n];
+        
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+            // Be careful about the "Start" point and assign the accessible result to 1 instead if 0
+                if (i == 0 && j == 0 && obstacleGrid[i][j] != 1){
+                    result[i][j] = 1;
+                }else{
+                    result[i][j] = 0;
+                }
+                
+                if (obstacleGrid[i][j] == 0){
+                    if (i > 0) result[i][j] += result[i - 1][j];
+                    if (j > 0) result[i][j] += result[i][j - 1];
+                }
+            }
+        }
+        return result[m - 1][n - 1];
+    }  
+}U
