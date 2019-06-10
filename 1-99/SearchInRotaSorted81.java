ccbd61e43c6416a067e477e6f990966569907e15
chenlinyue <chenlinyue1@gmail.com>
2019-06-10 17:20:34 +0800

    Update SearchInRotaSorted81.java

--- 1-99/SearchInRotaSorted81.java
+++ 1-99/SearchInRotaSorted81.java
@@ -1,28 +1,28 @@
 public boolean search(int[] nums, int target) {
-        int l = nums.length;
-        if (l <= 0) return false;
-        int left = 0, right = l - 1;
-        int mid;
-        while (left <= right){
-            mid = (left + right) / 2;
-            if (nums[mid] == target) return true;
-            
-            // left side sorted
-            if (nums[mid] > nums[left]){
-                if (nums[mid] > target){
-                    if (nums[left] > target) left = mid + 1;
-                    else right = mid - 1;
-                }else left = mid + 1;
-            }
-            // right side sorted
-            else if (nums[mid] < nums[left]){
-                if (nums[mid] < target){
-                    if (nums[right] < target) right = mid - 1;
-                    else left = mid + 1;
-                }else{
-                    right = mid - 1;
-                }
-            }else left++;
+    int l = nums.length;
+    if (l <= 0) return false;
+    int left = 0, right = l - 1;
+    int mid;
+    while (left <= right){
+        mid = (left + right) / 2;
+        if (nums[mid] == target) return true;
+
+        // left side sorted
+        if (nums[mid] > nums[left]){
+            if (nums[mid] > target){
+                if (nums[left] > target) left = mid + 1;
+                else right = mid - 1;
+            }else left = mid + 1;
         }
-        return false;
+        // right side sorted
+        else if (nums[mid] < nums[left]){
+            if (nums[mid] < target){
+                if (nums[right] < target) right = mid - 1;
+                else left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }else left++;
     }
+    return false;
+}
