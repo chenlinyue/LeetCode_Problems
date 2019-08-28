f2cf6ddd4e990bc52f5f850311d0199ee4160827
chenlinyue <chenlinyue1@gmail.com>
2019-08-28 00:21:53 -0500

    Update AccountsMerge721.java

--- 700-799/AccountsMerge721.java
+++ 700-799/AccountsMerge721.java
@@ -48,3 +48,49 @@ class UnionFindSolution {
         return par.get(s) == s ? s : find(par.get(s), par);
     }
 }
+
+
+class DFSSolution {
+    // this is the DFS solution
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        // a graph is created to represent emails, emails are nodes and ther are connected if they show up in the same account
+        Map<String, Set<String>> graph = new HashMap<>();
+        Map<String, String> owner = new HashMap<>();
+        // first, create the graph and connect owner and email
+        for (List<String> acc : accounts){
+            for (int i = 1; i < acc.size(); i++){
+                owner.put(acc.get(i), acc.get(0));
+                // graph is being created
+                if (!graph.containsKey(acc.get(i))) graph.put(acc.get(i), new HashSet<String>());
+                if (i == 1) continue;
+                graph.get(acc.get(i)).add(acc.get(i - 1));
+                graph.get(acc.get(i - 1)).add(acc.get(i));
+            }
+        }
+        
+        Set<String> visited = new HashSet<>();
+        List<List<String>> res = new ArrayList<>();
+        
+        for (String email : owner.keySet()){
+            if (!visited.contains(email)){
+                List<String> line = new ArrayList<>();
+                dfs(email, graph, visited, line);
+                Collections.sort(line);
+                line.add(0, owner.get(email));
+                res.add(line);
+            }
+        }
+        return res;
+    }
+     
+    // function dfs get the all emails that belongs to the same owner together in the list
+    public void dfs(String email, Map<String, Set<String>> graph, Set<String> visited, List<String> list){
+        list.add(email);
+        visited.add(email);
+        for (String neighbour : graph.get(email)){
+            if (!visited.contains(neighbour)){
+                dfs(neighbour, graph, visited, list);
+            }
+        }
+    }
+}
